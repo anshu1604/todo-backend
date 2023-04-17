3a01d49b8720f8d45a56b83e3418ffb5de6b31bb
anshu1604 <engineeranshu1999@gmail.com>
2023-04-17 16:18:57 +0530

    text change

--- src/db/connection.js
+++ src/db/connection.js
@@ -11,7 +11,7 @@ export const connectToDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     }, () => {
-      console.log("database connected!");
+      console.log("todo db connected!");
       return mongoose;
   })
 }
